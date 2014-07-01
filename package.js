21816a37aa431e5e0d97332b10910b4fafca29e5
Eslam A. Hefnawy <eahefnawy@gmail.com>
2014-07-02 01:43:07 +0300

    filled out package.js

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  summary: "Reaction Stripe - Stripe Payment Module for Reaction Commerce"
+});
+
+Package.on_use(function (api, where) {
+  api.use([
+    "standard-app-packages",
+    "coffeescript"
+  ], ["client", "server"]);
+  api.use([
+    "iron-router",
+    "less",
+    "reaction-core"
+  ], ["client"]);
+
+  api.add_files([
+    "client/register.coffee",
+    "client/routing.coffee",
+
+    "client/templates/helloworld/helloworld.html",
+    "client/templates/helloworld/helloworld.coffee",
+    "client/templates/helloworld/helloworld.less",
+
+    "client/templates/dashboard/widget/widget.html",
+    "client/templates/dashboard/widget/widget.coffee",
+    "client/templates/dashboard/widget/widget.less"
+  ], ["client"]);
+});
